46273d4790afaa8b90c990552649d9d18f79b7ee
brenda127s <brendaserra100@gmail.com>
2024-04-09 12:52:09 -0700

    working on restart button

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 let restartBtn = document.getElementById("restart-btn");
 let boxes = Array.from(document.getElementsByClassName("box"));
-let playerX = document.getElementById("x-player");
-let playerO = document.getElementById("o-player");
+// let playerX = document.getElementById("x-player").value;
+// let playerO = document.getElementById("o-player").value;
+const playerX = "X";
+    const playerO = "O";
+    let currentPlayer = playerX;
+
+
 let spaces = Array(9).fill(null);
 
-    if (playerX && playerO) {
-        startGame();
-    } else {
-        alert("Please enter name");
-    }
+
+    // if (playerX || playerO === "") {
+    //     alert("Please enter name");
+    // }
 
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked));
 
 }
 function boxClicked(e) {
-    console.log(e.target)
+    
+    const id = e.target.id
+
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer
+        e.target.innerText = currentPlayer
+
+        currentPlayer = currentPlayer == playerX ? playerO : playerX
+    }  
+}
+
+function restartGame() {
+    restartBtn.addEventListener('click', () => {
+        spaces.fill(null)
+        boxes.forEach(box => {
+            box.innerText = "";
+        })
+    })
 }
 
 startGame();
