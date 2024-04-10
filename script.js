6693ffe284f74ada594baf2deed1d8426667210a
brenda127s <brendaserra100@gmail.com>
2024-04-10 14:25:32 -0700

    game complete

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 let restartBtn = document.getElementById("restart-btn");
 let boxes = Array.from(document.getElementsByClassName("box"));
-// let playerX = document.getElementById("x-player").value;
-// let playerO = document.getElementById("o-player").value;
 const playerX = "X";
-    const playerO = "O";
-    let currentPlayer = playerX;
+const playerO = "O";
+let currentPlayer = playerX;
 
+
+const playerWinner = document.createElement("h2");
 
 let spaces = Array(9).fill(null);
 
-
-    // if (playerX || playerO === "") {
-    //     alert("Please enter name");
-    // }
 
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked));
@@ -26,17 +22,49 @@ function boxClicked(e) {
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
 
+        if (playerHasWon() !==false) {
+            playerWinner.innerHTML = `${currentPlayer} has won!`
+            playerWinner.classList.add("winner");
+
+            const parent = document.querySelector(".container");
+            parent.appendChild(playerWinner);
+
+        }
+
         currentPlayer = currentPlayer == playerX ? playerO : playerX
     }  
+restartGame();
+}
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition
+        if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a, b, c]
+        }
+    }
+    return false
 }
 
 function restartGame() {
     restartBtn.addEventListener('click', () => {
         spaces.fill(null)
-        boxes.forEach(box => {
+        boxes.forEach( box => {
             box.innerText = "";
         })
+        currentPlayer = playerX;
+        playerWinner.remove();
     })
 }
-
 startGame();
